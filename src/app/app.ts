624fa3b344bf93c8bcc91fbd2348fc3934374e39
João Amaro Lagedo <jlagedo@gmail.com>
2025-12-25 18:22:33 -0300

    REFACTOR

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import {BreadCrumbComponent} from './layout/bread-crumb-component/bread-crumb-component';
+import {ToastsContainer} from './layout/toasts-container/toasts-container';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, BreadCrumbComponent],
+  imports: [RouterOutlet, Header, BreadCrumbComponent, ToastsContainer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
